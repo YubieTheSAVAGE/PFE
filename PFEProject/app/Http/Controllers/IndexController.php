45982d45c8880f5b85b3d26fdceecfabdc28488b
Yubie <ayoub04bd@gmail.com>
2023-03-20 02:38:09 +0000

    parsed a message from laravel to vue

--- PFEProject/app/Http/Controllers/IndexController.php
+++ PFEProject/app/Http/Controllers/IndexController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Psr7\Message;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index(){
-        return inertia('Index/Index');
+        return inertia(
+            'Index/Index',
+            [
+                'message' => 'Hello from Laravel!'
+            ]
+        );
     }
     public function show(){
         return inertia('Index/Show');
--- PFEProject/resources/js/Pages/Index/Index.vue
+++ PFEProject/resources/js/Pages/Index/Index.vue
@@ -1,8 +1,12 @@
 <template>
     <div>This is Index!</div>
+    <p>this is the message: {{ message }}</p>
     <Link href="/hello">Show page</Link>
 </template>
 
 <script setup>
     import { Link } from '@inertiajs/vue3';
+    defineProps({
+        message: String // type of data expected to be parsed from laravel
+    })
 </script>
