9d4db8ab22ff79b8c67361ac67103a74a3653d62
Yubie <ayoub04bd@gmail.com>
2023-03-20 02:57:25 +0000

    added a ref object

--- PFEProject/resources/js/Layouts/MainLayout.vue
+++ PFEProject/resources/js/Layouts/MainLayout.vue
@@ -1,9 +1,16 @@
 <template>
     <Link href="/hello">Show page</Link>&nbsp;
     <Link href="/">Index page</Link>
+    <div>Time spent: {{ timer }}s</div>
     <slot></slot>
 </template>
 
 <script setup>
     import { Link } from '@inertiajs/vue3';
+    import { ref } from 'vue';
+
+    const timer = ref(0) ;
+    setInterval(function(){
+        timer.value++
+    },1000)
 </script>
